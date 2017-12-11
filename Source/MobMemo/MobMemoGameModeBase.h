fdb0d4773e9d40565bfc257a6de7264a872614de
Ginosik <luis.ega@hotmail.com>
2017-12-11 12:05:43 -0200

    first

---
+++ Source/MobMemo/MobMemoGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MobMemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOBMEMO_API AMobMemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/MobMemo/MyController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MyController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MOBMEMO_API AMyController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
